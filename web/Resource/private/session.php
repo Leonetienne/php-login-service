9d46c5339a7c0e0b2bb6b0988647f7b842ed2dfb
Leonetienne <leonetienne@hotmail.de>
2021-03-04 23:21:48 +0100

    Implemented bumpSession method

--- web/Resource/private/session.php
+++ web/Resource/private/session.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'database.php';
 
+// Will create a unique session for an account
+// Returns either false or a sessionId as a string
 function CreateSession($email) {
 	// Connect to database
 	$conn = ConnectToDatabase();
@@ -27,7 +29,7 @@ function CreateSession($email) {
 	// Create fe_users entry
 	$results = SecureQuery(
 		$conn,
-		"INSERT INTO ses_ids (sesId, accountId, time_created, time_expires) VALUES (?, ?, ?, ?)",
+		"INSERT INTO ses_ids (sesId, accountId, time_created, time_expires) VALUES (?, ?, ?, ?);",
 		"siii",
 		$sesId,
 		$uid,
@@ -39,4 +41,48 @@ function CreateSession($email) {
 	return $sesId;
 }
 
+// Will bump a session
+// If the session is still active, it will renew it
+// If the session is expired, it will remove it
+// Returns true, if the session is still good. Either false.
+function BumpSession($sessionId) {
+	// Connect to database
+	$conn = ConnectToDatabase();
+
+	// Fetch expiry date
+	$results = SecureQuery(
+		$conn,
+		"SELECT time_expires FROM ses_ids WHERE sesId = ?;",
+		"s",
+		$sessionId
+	)->fetch_all();
+
+	// Was any session found?
+	if (count($results) < 1) {
+		$conn->close();
+		return false;
+	}
+
+	// If session is expired
+	if ($results[0][0] < time()) {
+		SecureQuery(
+			$conn,
+			"DELETE FROM ses_ids WHERE sesId = ?;",
+			"s",
+			$sessionId
+		);
+		return false;
+	}
+
+	// Renew expiry date
+	SecureQuery(
+		$conn,
+		"UPDATE ses_ids SET time_expires = ? WHERE sesId = ?;",
+		"is",
+		time() + 60*60*24, // Expires after one day
+		$sessionId
+	);
+
+	return true;
+}
 ?>
